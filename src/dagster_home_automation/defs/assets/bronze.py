cc54934a6fdb1492cdd69966e83ee314089953f0
Laurence MacGuire <leprechaun@gmail.com>
2026-06-23 21:32:46 +0700

    fix defs

--- src/dagster_home_automation/defs/assets/bronze.py
+++ src/dagster_home_automation/defs/assets/bronze.py
@@ -2,9 +2,9 @@ import polars as pl
 from dagster import AutomationCondition, Definitions, AssetExecutionContext, asset, EnvVar
 
 @asset(
-    group_name="home_automation_raw",
-    io_manager_key="home_automation_raw_io_manager",
-    key_prefix="home_automation_raw",
+    group_name="home_automation_bronze",
+    io_manager_key="home_automation_bronze_io_manager",
+    key_prefix="home_automation_bronze",
     automation_condition=AutomationCondition.eager(),
 )
 def electricity(context: AssetExecutionContext) -> pl.DataFrame:
