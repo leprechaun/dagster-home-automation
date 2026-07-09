5e76010d5e085a176a8c564dd9b7b462b61d9535
Laurence MacGuire <leprechaun@gmail.com>
2026-07-09 23:08:57 +0700

    Add the openlineage sensor

--- src/dagster_home_automation/defs/resources.py
+++ src/dagster_home_automation/defs/resources.py
@@ -2,13 +2,17 @@
 from dagster import Definitions, EnvVar
 from dagster_deltalake import S3Config
 from dagster_deltalake_polars import DeltaLakePolarsIOManager
+from dagster_openlineage import openlineage_sensor
 
 
 _s3_config = S3Config(allow_unsafe_rename=True, endpoint=EnvVar("AWS_ENDPOINT_URL_S3"))
 
-defs = Definitions(resources={
-    "home_automation_io_manager": DeltaLakePolarsIOManager(
-        root_uri="s3://deltalake/home-automation/",
-        storage_options=_s3_config,
-    )
-})
+defs = Definitions(
+    sensors=[openlineage_sensor(include_asset_events=True)],
+    resources={
+        "home_automation_io_manager": DeltaLakePolarsIOManager(
+            root_uri="s3://deltalake/home-automation/",
+            storage_options=_s3_config,
+        )
+    }
+)
